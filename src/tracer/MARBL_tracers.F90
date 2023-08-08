f2eae664b59fa16ef611b31d9c4cf743486377a5
Michael Levy <mike.levy.work@gmail.com>
2023-08-08 11:10:00 -0600

    Updates to use support_mks branch of MARBL
    This branch of MARBL requires the unit_system argument in a few places;
    sticking with cgs introduces a few round-off level differences due to
    restructing some internal MARBL computations.

--- src/tracer/MARBL_tracers.F90
+++ src/tracer/MARBL_tracers.F90
@@ -304,6 +304,7 @@ subroutine configure_MARBL_tracers(GV, param_file, CS)
                             gcm_delta_z = GV%sInterface(2:nz+1) - GV%sInterface(1:nz), &
                             gcm_zw = GV%sInterface(2:nz+1), &
                             gcm_zt = GV%sLayer, &
+                            unit_system = "cgs", &
                             lgcm_has_global_ops = .true. &
                            )
   if (MARBL_instances%StatusLog%labort_marbl) &
@@ -318,6 +319,7 @@ subroutine configure_MARBL_tracers(GV, param_file, CS)
   CS%sfo_cnt = CS%sfo_cnt +1
   call MARBL_instances%surface_flux_output%add_output(num_elements=1, &
                                                       field_name="flux_co2", &
+                                                      unit_system="cgs", &
                                                       output_id=CS%flux_co2_ind, &
                                                       marbl_status_log=MARBL_instances%StatusLog)
 
