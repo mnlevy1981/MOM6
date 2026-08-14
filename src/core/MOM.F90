97e5e4b0a64cec1d3e1c471746e6572dc78fd37c
kdraeder <raeder@ucar.edu>
2026-08-14 03:10:12 -0600

    Increased file name size (#435)
    src/core/MOM.F90
    src/framework/MOM_restart.F90
      Handle CESM test names with multiple test_mods.
      Was needed during low-res MOM6 and CESM3 low-res tests.

--- src/core/MOM.F90
+++ src/core/MOM.F90
@@ -362,7 +362,7 @@ type, public :: MOM_control_struct ; private
 
   ! Variables needed to reach between start and finish phases of initialization
   logical :: write_IC           !< If true, then the initial conditions will be written to file
-  character(len=120) :: IC_file !< A file into which the initial conditions are
+  character(len=512) :: IC_file !< A file into which the initial conditions are
                                 !! written in a new run if SAVE_INITIAL_CONDS is true.
 
   logical :: calc_rho_for_sea_lev !< If true, calculate rho to convert pressure to sea level
